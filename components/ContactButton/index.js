35d93d22200d5c3d8f95b26b97a36754a94c3127
Nacho Varni <nachovarni6@gmail.com>
2021-07-22 21:07:16 -0300

    details

--- components/ContactButton/index.js
+++ components/ContactButton/index.js
@@ -3,10 +3,8 @@ import styles from "./ContactButton.module.scss";
 
 export default function ContactButton() {
   return (
-    <span className={styles.contactButton}>
-      <Link to="contactMenu" spy={true} smooth={true}>
-        CONTACTANOS!
-      </Link>
-    </span>
+    <Link to="contactMenu" spy={true} smooth={true}>
+      <span className={styles.contactButton}>CONTACTANOS!</span>
+    </Link>
   );
 }
--- components/Navbar/index.js
+++ components/Navbar/index.js
@@ -3,8 +3,13 @@ import styles from "./Navbar.module.scss";
 import Image from "next/image";
 import Logo from "../../public/assets/logo-dos-orillas.png";
 import { Link } from "react-scroll";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { useState } from "react";
 
 function Navbar() {
+  const [hamMenu, setHamMenu] = useState(false);
+
   return (
     <div className={styles.navbar}>
       <div className={styles.logoWrapper}>
@@ -14,22 +19,45 @@ function Navbar() {
       </div>
 
       <ul className={styles.list}>
-        <Link to="about" spy={true} smooth={true}>
+        <Link to="about" spy={true} smooth={true} activeClass={styles.active}>
           <li className={styles.item}>CONOCENOS</li>
         </Link>
-        <Link to="explain" spy={true} smooth={true}>
+        <Link to="explain" spy={true} smooth={true} activeClass={styles.active}>
           <li className={styles.item}>QUE HACEMOS</li>
         </Link>
-        <Link to="work" spy={true} smooth={true}>
+        <Link to="work" spy={true} smooth={true} activeClass={styles.active}>
           <li className={styles.item}>NUESTRO TRABAJO</li>
         </Link>
-        <Link to="budget" spy={true} smooth={true}>
+        <Link to="budget" spy={true} smooth={true} activeClass={styles.active}>
           <li className={styles.item} to="budget">
             PRESUPUESTA
           </li>
         </Link>
       </ul>
-      <div className={styles.hamburguerMenu}>hamburguer</div>
+      <div
+        className={styles.hamburguerMenu}
+        onClick={() => setHamMenu((prevState) => !prevState)}
+      >
+        <FontAwesomeIcon icon={faBars} className={styles.hamburguer} />
+      </div>
+      {hamMenu && (
+        <ul className={styles.hamList}>
+          <Link to="about" spy={true} smooth={true} activeClass={true}>
+            <li className={styles.hamItem}>CONOCENOS</li>
+          </Link>
+          <Link to="explain" spy={true} smooth={true} activeClass={true}>
+            <li className={styles.hamItem}>QUE HACEMOS</li>
+          </Link>
+          <Link to="work" spy={true} smooth={true} activeClass={true}>
+            <li className={styles.hamItem}>NUESTRO TRABAJO</li>
+          </Link>
+          <Link to="budget" spy={true} smooth={true} activeClass={true}>
+            <li className={styles.hamItem} to="budget">
+              PRESUPUESTA
+            </li>
+          </Link>
+        </ul>
+      )}
     </div>
   );
 }
