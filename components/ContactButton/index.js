eb7b34f79973c2eb346f90ab9eabf0840f35cad9
Nacho Varni <nachovarni6@gmail.com>
2021-07-24 13:58:03 -0300

    whatsapp button

--- components/ContactButton/index.js
+++ components/ContactButton/index.js
@@ -1,10 +1,19 @@
 import { Link } from "react-scroll";
 import styles from "./ContactButton.module.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faWhatsapp } from "@fortawesome/free-brands-svg-icons";
 
 export default function ContactButton() {
   return (
-    <Link to="contactMenu" spy={true} smooth={true}>
-      <span className={styles.contactButton}>CONTACTANOS!</span>
-    </Link>
+    <>
+      <a
+        href="https://wa.me/541164091233"
+        className={styles.whatsappFloat}
+        // target="_blank"
+        // rel="noopener noreferrer"
+      >
+        <FontAwesomeIcon icon={faWhatsapp} />
+      </a>
+    </>
   );
 }
